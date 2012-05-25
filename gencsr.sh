f37e78f2996bfe12ea12a775f18d5025249c9b5d
Stewart Rutledge <stew.rutledge@gmail.com>
2012-05-25 14:05:05 +0200

    Made the gencsr script non interactive, opting for command line options instead.

--- gencsr.sh
+++ gencsr.sh
@@ -2,39 +2,102 @@
 # A simple bash script for generating CSRs in which the only difference in info will be the Common Name
 # Define the various variables to your fitting and just run the script, it will prompt you for the CN
 
-Location=YourLocation
-Country=EnterCountryHere
-State=EnterStateHere
-Org=EnterOrgnaztionHere
-OrgUnit=YourOU
+which blarg
+check_openssl=$(echo $?)
+if [[ $check_openssl == 1 ]]; then
+  echo "openssl binary could not be found. Make sure it installed and found in your \$PATH"
+  exit 1
+fi
+ 
 
- function gencsr {
-                if [ -f $domain.key ];
-                        then
-                echo -e "\033[38;5;148m$domain.key found, generating a new csr\033[39m"
-                openssl req -new -key $domain.key -out ./$domain.csr -batch -subj "/C=$Country/ST=$State/L=Stockholm/O=$Org/OU=$OrgUnit/CN=$domain"
-                        else
-                echo -e "\033[38;5;148mNo key found, generating one now\033[39m"
-                openssl genrsa -des3 -out $domain.key 2048;
-                echo -e "\033[38;5;148mGenerating passwordless key, enter password used during key creation\033[39m"
-                openssl rsa -in $domain.key -out $domain.key.insecure;
-                mv $domain.key $domain.key.secure;
-                mv $domain.key.insecure $domain.key;
-                openssl req -new -key $domain.key -out ./$domain.csr -batch -subj "/C=$Country/ST=$State/L=Stockholm/O=$Org/OU=$OrgUnit/CN=$domain/CN=$domain"
-                fi      }
-
-
-echo "Quick CSR creation with no spelling mistakes"
-echo -n "Enter Domain Name: "
-read domain
-
-echo -e "\033[32mThis script will attempt to create a key and an associated CSR. If there is a key file with the name $domain.key then it will use that for the CSR.\033[39m"
-echo -e "You have entered: \033[31m$domain\033[39m"
-while true; do
-    read -p "Is this correct and do you wish to continue? [Y/N] "  yn
-    case $yn in
-        [Yy]* ) gencsr; break;;
-        [Nn]* ) exit;;
-        * ) echo "This is a yes or no question, try again";;
-    esac
+while getopts ":l:c:s:o:u:d:i:" opts; do
+  case $opts in
+    l)
+      location=$OPTARG
+      ;;
+    c)
+      country=$OPTARG
+      ;;
+    s)
+      state=$OPTARG
+      ;;
+    o)
+      org=$OPTARG
+      ;;
+    u)
+      orgunit=$OPTARG
+      ;;
+    d)
+      domain=$OPTARG
+      ;;
+    i)
+      insecure=$OPTARG
+      ;;
+   esac
 done
+
+function usage {
+  echo "$0 [-l -c -s -o -u -d -i [y]
+  -l Location
+  -c Two Letter Country Code Country
+  -s State
+  -o Organization
+  -u Organizational Unit
+  -d Domain
+  -i Generate insecure key (-i y)
+  "
+} 
+
+
+
+function gen_key {
+  echo -e "\033[38;5;148mGenerating key with $domain\033[39m"
+  openssl genrsa -des3 -out $domain.key 2048;
+}
+
+
+
+function nopasswd_key (){
+  echo -e "\033[38;5;148mGenerating passwordless key, enter password used during key creation\033[39m"
+  openssl rsa -in $domain.key -out $domain.key.insecure;
+  mv $domain.key $domain.key.secure;
+  mv $domain.key.insecure $domain.key;
+}
+
+function gen_csr (){
+  if [[ $insecure == "y" ]]; then
+    echo -e "\033[38;5;148mGenerating CSR\033[39m"
+  else
+    echo -e "\033[38;5;148mGenerating CSR, enter password used during key generation\033[39m"
+  fi
+  openssl req -new -key $domain.key -out ./$domain.csr -batch -subj "/C=$country/ST=$state/L=$location/O=$org/OU=$orgunit/CN=$domain/CN=$domain"
+}
+
+if [[ -z "$location" ]] || [[ -z "$country" ]] || [[ -z "$state" ]] || [[ -z "$org" ]] || [[ -z "$orgunit" ]] || [[ -z "$domain" ]]; then
+  usage
+  exit 1
+fi
+
+if [[ "$country" != [A-Za-z][A-Za-z] ]];then
+  echo "Please use two letter country code"
+  exit 1
+fi
+
+if [[ -z $insecure ]]; then
+  gen_key
+  gen_csr
+  echo -e "\033[38;5;148mDone!\033[39m"
+  exit 0
+fi
+
+if [[ $insecure == "y" ]]; then
+  gen_key
+  nopasswd_key
+  gen_csr
+  echo -e "\033[38;5;148mDone!\033[39m"
+  exit 0
+else
+  echo "Please specify "y" and only "y" if you want a key with no password"
+  exit 1
+fi
+
